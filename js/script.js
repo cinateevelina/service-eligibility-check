41311ce7bd4c4bbf17de75f7d223c3e7d97ab241
cinateevelina <cinateevelina@gmail.com>
2026-09-23 19:51:58 +0200

    ændringer

--- js/script.js
+++ js/script.js
@@ -1,21 +1,42 @@
 // Husk fra dag 1: skriv "use strict" herunder
-
+"use strict";
 
 function tjekGratisService(brand, year) {
 
     // let, fordi værdien kan ændre sig nedenfor
+
+
     let harGratisService = false;
+
+     if ((brand === `Volkswagen` || brand === `Skoda`) && (year> 2023) ) { 
+
+        harGratisService = true;
+        console.log(`${brand} fra ${year} får gratis service i 1 år.`);
+
+} else {
+    harGratisService = false;
+    console.log(`${brand} fra ${year} får ikke gratis service.`);
+}
+
+}
+
+// Volkswagen fra 2024 får gratis service i 1 år 
+// Skoda fra 2020 får ikke gratis service 
+// Tesla fra 2025 får ikke gratis service
+
+
+
 
     // Skriv if/else-strukturen selv herinde, ligesom i klasseøvelsen.
     //
     // Hvis (brand er "Volkswagen" ELLER brand er "Skoda") OG year er større end 2023, så:
-    //   - sæt harGratisService til true
+    //   - sæt harGratisService til true 
     //   - log `${brand} fra ${year} får gratis service i 1 år.`
     // Ellers:
     //   - sæt harGratisService til false
     //   - log `${brand} fra ${year} får ikke gratis service.`
 
-}
+
 
 tjekGratisService(`Volkswagen`, 2024);
 tjekGratisService(`Skoda`, 2020);
